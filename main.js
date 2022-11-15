1b8d9e0f773799e9316abd9949c506384924fd92
unknown <camilo2251@gmail.com>
2022-11-15 17:38:26 -0500

    Menu-desktop-mobile

--- main.js
+++ main.js
@@ -1,9 +1,16 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
-const body = document.querySelector('body');
+const mobileMenu = document.querySelector('.mobile-menu');
+const menu = document.querySelector('.menu');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
+menu.addEventListener('click', toggleMobileMenu);
 
 function toggleDesktopMenu(){
-    desktopMenu. classList.toggle('inactive')
+    desktopMenu.classList.toggle('inactive')
+}
+
+
+function toggleMobileMenu(){
+    mobileMenu.classList.toggle('inactive')
 }
